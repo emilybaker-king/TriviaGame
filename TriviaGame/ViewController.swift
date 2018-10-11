0728ce0c85f8129d1ded4eaa2b49d8b48749a3f7
Emily Baker-King <ebakerking@gmail.com>
2018-10-11 08:01:23 -0500

    Questions

--- TriviaGame/ViewController.swift
+++ TriviaGame/ViewController.swift
@@ -10,12 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var welcomeLabel: UILabel!
+    @IBOutlet weak var intro: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("Welcome")
+        
+        //Label that welcomes the users
+        welcomeLabel.text = "Welcome to the Trivia Game"
+        
+        //Label that describes what the game is about
+        intro.text = "This is a game of random trivia, so let's see what kind of stuff you know!"
+        
     }
 
-
+    
 }
 
